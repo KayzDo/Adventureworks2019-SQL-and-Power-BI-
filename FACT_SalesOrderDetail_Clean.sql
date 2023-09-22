857ae26be826a0bf710fcef3a28db60bad7b60d0
Khanh Do Ngoc <141127437+KayzDo@users.noreply.github.com>
2023-09-22 16:53:42 +0700

    Create FACT_SalesOrderDetail_Clean.sql

---
+++ FACT_SalesOrderDetail_Clean.sql
@@ -0,0 +1,14 @@
+-- Cleansed FACT_SalesOrderDetail Table --
+SELECT 
+  SalesOrderID,
+  SalesOrderDetailID,
+  -- CarrierTrackingNumber,
+  OrderQty,
+  ProductID,
+  SpecialOfferID,
+  UnitPrice,
+  UnitPriceDiscount,
+  LineTotal,
+  -- rowguid,
+  ModifiedDate
+FROM `adventureworks2019.Sales.SalesOrderDetail` 
